0c466a8d76ef8293412beca3260e4611546d6102
Kip-Hub <daniel.kip@hva.nl>
2021-11-11 13:59:52 +0100

    added: transition and color scale

--- static/index.js
+++ static/index.js
@@ -1,7 +1,7 @@
-const margin = { top: 40, bottom: 10, left: 120, right: 20 };
-const width = 800 - margin.left - margin.right;
-const height = 1200 - margin.top - margin.bottom;
-const seasons = 5;
+const margin = { top: 40, bottom: 10, left: 160, right: 20 };
+let width = 800 - margin.left - margin.right;
+let height = 800 - margin.top - margin.bottom;
+
 
 // Creates sources <svg> element
 const svg = d3.select('body').append('svg')
@@ -14,14 +14,18 @@ const g = svg.append('g')
 let data;
 
 const xscale = d3.scaleLinear().range([0, width]);
-const yscale = d3.scaleBand().rangeRound([0, height]).paddingInner(0.4);
+const yscale = d3.scaleBand().rangeRound([0, height]).paddingInner(0.15);
 
 const xaxis = d3.axisTop().scale(xscale);
 const g_xaxis = g.append('g').attr('class', 'x axis');
 const yaxis = d3.axisLeft().scale(yscale);
 const g_yaxis = g.append('g').attr('class', 'y axis');
 
-d3.json('https://api.tvmaze.com/shows/169/episodes')
+const color = d3.scaleOrdinal()
+    .domain(["a", "b", "c", "d", "e"])
+    .range(d3.schemeDark2);
+
+d3.json('https://api.tvmaze.com/shows/82/episodes')
     .then((json) => {
         data = json;
         update(data);
@@ -39,28 +43,38 @@ function update(new_data) {
     // Render the chart with new data
 
     // DATA JOIN use the key argument for ensurign that the same DOM element is bound to the same data-item
-    const rect = g.selectAll('rect').data(new_data, (d) => d.name).join(
-        // ENTER 
-        // new elements
-        (enter) => {
-            const rect_enter = enter.append('rect').attr('x', 0);
-            rect_enter.append('title');
-            return rect_enter;
-        },
-        // UPDATE
-        // update existing elements
-        (update) => update,
-        // EXIT
-        // elements that aren't associated with data
-        (exit) => exit.remove()
-    );
+    const rect = g.selectAll('rect')
+        .data(new_data, (d) => d.name).join(
+            // ENTER 
+            // new elements
+            (enter) => {
+                const rect_enter = enter
+                    .append('rect')
+                    .attr('x', 0)
+                    .attr('fill', (d) => { return (color(d.name)) });
+                rect_enter.append('title');
+                return rect_enter;
+            },
+            // UPDATE
+            // update existing elements
+            (update) => update,
+            // EXIT
+            // elements that aren't associated with data
+            (exit) => exit.remove()
+        );
 
     // ENTER + UPDATE
     // both old and new elements
-    rect.transition()
+    rect
         .attr('height', yscale.bandwidth())
-        .attr('width', (d) => xscale(d.rating.average))
-        .attr('y', (d) => yscale(d.name));
+        .attr('y', (d) => yscale(d.name))
+        .transition()
+        .duration(400)
+        .ease(d3.easeBackOut.overshoot(1.7))
+        .delay((d, i) => {
+            return i * 10
+        })
+        .attr('width', d => xscale(d.rating.average))
 
     rect.select('title').text((d) => d.rating.average);
 }
